1cc8f0fc4a381a8a5cf3f7407db9a1b8e58e44f7
takapamu <takapamukyary0129@gmail.com>
2021-07-20 22:34:02 +0900

    add Chapter04.java

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+	public static void main(String[] args)
+	{
+		final int LUCKY_NUMBER = 19;
+		String str1 = "takato matsumoto";;
+		System.out.println(str1);
+		
+		str1 = "BiSH";
+		System.out.println(str1);
+		
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
