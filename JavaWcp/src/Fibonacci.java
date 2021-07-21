8d52dac4e41d60583eb785d6759422997689db1d
takapamu <takapamukyary0129@gmail.com>
2021-07-21 22:16:56 +0900

    add Fibonacci

---
+++ JavaWcp/src/Fibonacci.java
@@ -0,0 +1,25 @@
+
+public class Fibonacci {
+	public static void main(String[] args)
+	{
+		int f0,f1,f2;
+		
+		f0 = 0;
+		f1 = 1;
+		
+		System.out.println(f0);
+		
+		while(f1 < 100000)
+		{
+			System.out.println(f1);
+			
+			f2 = f0 + f1;
+			
+			f0 = f1;
+			
+			f1 = f2;
+		}
+		
+	}
+
+}
