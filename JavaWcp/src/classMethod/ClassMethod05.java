bd3e8166c934040eaf7ca5b3c06bb77cd49aaf66
takapamu <takapamukyary0129@gmail.com>
2021-07-21 19:45:45 +0900

    add ClassMethod05 Human05

---
+++ JavaWcp/src/classMethod/ClassMethod05.java
@@ -0,0 +1,13 @@
+package classMethod;
+
+import classMethod.human.Human05;
+
+public class ClassMethod05 {
+	public static void main(String[] args)
+	{
+		Human05 sento = new Human05();
+		
+		System.out.println("名前は" + sento.getName() + "で、年齢は" + sento.getAge() + "です。");
+	}
+
+}
---
+++ JavaWcp/src/classMethod/human/Human05.java
@@ -0,0 +1,22 @@
+package classMethod.human;
+
+public class Human05 {
+	private String name;
+	private int age;
+	
+	public Human05()
+	{
+		name = "セントチヒロ・チッチ";
+		age = 22;
+	}
+	
+    public String getName()
+    {
+    	return name;
+    }
+    
+    public int getAge()
+    {
+    	return age;
+    }
+}
