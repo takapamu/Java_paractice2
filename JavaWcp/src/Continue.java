fd37777375b5d56e8a3d1e0320ca51cfaa135d57
takapamu <takapamukyary0129@gmail.com>
2021-07-21 17:42:42 +0900

    add continue Recursive

---
+++ JavaWcp/src/Continue.java
@@ -0,0 +1,15 @@
+
+public class Continue {
+	public static void main(String[] args)
+	{
+		for (int count = 0; count <= 4; count++)
+		{
+			if(count == 2)
+			{
+				continue;
+			}
+			System.out.println("Continue01 = " + count);
+		}
+	}
+
+}
