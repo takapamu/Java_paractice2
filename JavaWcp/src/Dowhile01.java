5f75b6a1589ce84a4ea59364c194ba2ad9ca55a6
takapamu <takapamukyary0129@gmail.com>
2021-07-21 17:30:23 +0900

    add do while,for

---
+++ JavaWcp/src/Dowhile01.java
@@ -0,0 +1,14 @@
+
+public class Dowhile01 {
+	public static void main(String[] args)
+	{
+		int number = 1;
+		do
+		{
+			number *= 2;
+			System.out.println("DoWhile01 = "  + number);
+			
+		} while(number < 50);
+	}
+
+}
