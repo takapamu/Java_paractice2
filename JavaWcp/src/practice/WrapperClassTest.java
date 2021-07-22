48eef15271b83fd4bc8b44bdaaff8248bca09186
takapamu <takapamukyary0129@gmail.com>
2021-07-22 14:51:18 +0900

    add WrapperClassTest

---
+++ JavaWcp/src/practice/WrapperClassTest.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class WrapperClassTest {
+	public static void main(String[] args)
+	{
+		Integer integer1  = new Integer("2021");
+		String str1 = integer1.toString();
+		System.out.println(str1);
+	}
+
+}
